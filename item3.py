7284b5aabe53e44aaabf3a1b49ce879caeff67b1
matixaxs <ma.melladoh@duocuc.cl>
2026-04-08 17:11:22 +0000

    Subida final

---
+++ item3.py
@@ -0,0 +1,16 @@
+import simplejson as json
+import time
+
+with open('myfile.json','r') as file:
+    variable_json = json.load(file)
+
+print("Contenido de la variable:", variable_json)
+
+token = variable_json['token']
+expiracion = variable_json['expires_at']
+tiempo_actual = time.time()
+tiempo_restante =expiracion - tiempo_actual
+
+print("Token:", token)
+print("Tiempo restante (segundos):", int(tiempo_restante))
+
